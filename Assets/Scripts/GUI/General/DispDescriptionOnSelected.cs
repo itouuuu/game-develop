551d30dd5563ba915707e2c69156114ee79b65cf
UNIQAbes <uniqabes@gmail.com>
2024-03-23 18:41:03 +0900

    簡易的に見た目だけ作った

---
+++ Assets/Scripts/GUI/General/DispDescriptionOnSelected.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+[RequireComponent(typeof(EventTrigger))]
+public class DispDescriptionOnSelected : MonoBehaviour
+{
+    [SerializeField] private MessageStackDescription _messageStackDescription;
+    //[SerializeField] private VSModeLobbyUI _lobbyUI;
+    [SerializeField] private EventTrigger _eventTrigger;
+    //[SerializeField, TextArea(1, 5)] public string _dispMessage= "‚ ‚¢‚¤‚¦‚¨abcdeABCDE";
+    [SerializeField, TextArea(1, 5)] public string DispMessage = "‚ ‚¢‚¤‚¦‚¨abcdeABCDE";
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _eventTrigger.AddOnSelected(OnSelect);
+        _eventTrigger.AddOnDeselected(OnDeselect);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSelect(BaseEventData baseEventData)
+    {
+        _messageStackDescription.DispNewDescription(this.gameObject, DispMessage);
+    }
+    public void OnDeselect(BaseEventData baseEventData)
+    {
+        _messageStackDescription.DeleteDescription(this.gameObject);
+    }
+
+
+
+    private void Reset()
+    {
+        /*
+        GameObject lobbyUIObj = GameObject.Find("UIM");
+        if (lobbyUIObj)
+        {
+            _lobbyUI = lobbyUIObj.GetComponent<VSModeLobbyUI>();
+        }
+        */
+
+        GameObject descriptionObj = GameObject.Find("Description");
+        if (descriptionObj)
+        {
+            _messageStackDescription = descriptionObj.GetComponent<MessageStackDescription>();
+        }
+
+        _eventTrigger = GetComponent<EventTrigger>();
+
+    }
+}
