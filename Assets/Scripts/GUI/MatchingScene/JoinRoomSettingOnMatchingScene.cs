b5f69106f6255054f9fabfbed8fd398d834158a2
UNIQAbes <uniqabes@gmail.com>
2024-03-25 17:39:11 +0900

    合言葉が反映されるようにした

--- Assets/Scripts/GUI/MatchingScene/JoinRoomSettingOnMatchingScene.cs
+++ Assets/Scripts/GUI/MatchingScene/JoinRoomSettingOnMatchingScene.cs
@@ -45,16 +45,12 @@ public class JoinRoomSettingOnMatchingScene : MonoBehaviourPunCallbacks
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        Debug.Log($"ルームリストが更新されました{roomList.Count}");
-        if (roomList.Count > 0) 
-        {
-            Debug.Log($"RoomName:{roomList[0].Name}");
-        }
+        Debug.Log($"ルームリストが更新されました:{roomList.Count}");
+
 
         foreach (var aRoomInfo in roomList)
         {
             int aRoomIndex = _roomInfos.FindIndex((aData) => { return aData.Name == aRoomInfo.Name; });
-            Debug.Log(!aRoomInfo.IsVisible || !aRoomInfo.IsOpen || aRoomInfo.RemovedFromList);
             if (!aRoomInfo.IsVisible || !aRoomInfo.IsOpen || aRoomInfo.RemovedFromList)//部屋がリスト入っているべきで無いなら
             {
                 if (aRoomIndex >= 0)//すでにリストに入っているなら
@@ -70,9 +66,14 @@ public class JoinRoomSettingOnMatchingScene : MonoBehaviourPunCallbacks
 
             if (aRoomIndex < 0)//リストに入っていないなら
             {
-                Debug.Log("インスタン市営としたよ～");
+                string keyword = "";
+                if (aRoomInfo.CustomProperties.ContainsKey("Keyword") && aRoomInfo.CustomProperties["Keyword"] is string) 
+                {
+                    keyword = (string)aRoomInfo.CustomProperties["Keyword"];
+                }
+                
                 RoomInfoOnMatchingScene newInfoObj = Instantiate(roomInfoPrefab, viewportContent); //部屋の情報を表示するGameObjectを作成
-                _roomInfos.Add(new Roominfo(aRoomInfo.Name, aRoomInfo.masterClientId, false, aRoomInfo.PlayerCount, newInfoObj));//部屋の情報を格納しているリストにその部屋の情報を追加
+                _roomInfos.Add(new Roominfo(aRoomInfo.Name, aRoomInfo.masterClientId, keyword!="", aRoomInfo.PlayerCount, newInfoObj));//部屋の情報を格納しているリストにその部屋の情報を追加
                 newInfoObj.SetValues(_roomInfos[_roomInfos.Count - 1]);//部屋の情報を表示するGameObjectに、情報を表示する為の情報を渡す
             }
             else    //すでにリストに入っているなら
