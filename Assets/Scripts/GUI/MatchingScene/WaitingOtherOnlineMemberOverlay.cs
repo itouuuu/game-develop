38bc41d498bc8260fa0541a29d129eabb51d68c4
UNIQAbes <uniqabes@gmail.com>
2024-03-27 23:10:59 +0900

    サーバー側でどのような状態でもMtchingSceneに入れるようにした

--- Assets/Scripts/GUI/MatchingScene/WaitingOtherOnlineMemberOverlay.cs
+++ Assets/Scripts/GUI/MatchingScene/WaitingOtherOnlineMemberOverlay.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class WaitingOtherOnlineMemberOverlay : MonoBehaviour
 {
     public TextMeshProUGUI messageTextMesh;
     public Button cancelButton;
+    public string BattleSceneName = "TestScene";
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,10 @@ public class WaitingOtherOnlineMemberOverlay : MonoBehaviour
         }
 
         messageTextMesh.text = $"‘¼‚ÌŽQ‰ÁŽÒ‚ð‘Ò‚Á‚Ä‚¢‚Ü‚·c({playerNum}/4)";
+        if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom.PlayerCount >= 4)
+        {
+            SceneManager.LoadScene(BattleSceneName);
+        }
     }
 
     public void OnCancelButtonClicked() 
